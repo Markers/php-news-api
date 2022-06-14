7b6e55df0097b0ad43bec2908f31b3d4ad3a4e71
Kyungseo-Park <troublesome.dev@gmail.com>
2022-06-14 23:51:32 +0900

    test: jetbrains의 예제 api 추가

--- routes/api.php
+++ routes/api.php
@@ -18,52 +18,24 @@ Route::prefix('v1')->group(function () {
 
     Route::get('/php-annotated-monthly', function (Request $request) {
         // jetbrains API
-        $aaa = [
-            'data' => [
-                [
-                    "href" => "https://blog.jetbrains.com/phpstorm/2022/05/php-annotated-may-2022/",
-                    "thubnamil" => [
-                        'src' => "https://blog.jetbrains.com/wp-content/uploads/2022/06/Blog_Featured_image_1280x600_PhpStorm-2x-1.png",
-                        'width'=> '2560',
-                        'height'=> '1200',
-                        'sizes' => '(max-width: 768px) 768px , 525px',
-                    ],
-                    "datetime" => '2022-06-13',
-                    "title" => 'PhpStorm 2022.2 EAP #3: Creating Enums',
-                    "description" => 'Welcome back to the 2022.2 EAP series! If you’re new or unsure what our Early Access Program is, make sure to read th…',
-                    "writer_img" => 'https://secure.gravatar.com/avatar/cef66d348f7def8f4634963a7e7a05e4?s=200&r=g',
-                    "writer" => 'Brent Roose',
+        $data['data'] = [];
+        for ($i=1; $i < 20; $i++) {
+            array_push($data['data'], [
+                "id" => $i,
+                "href" => "https://blog.jetbrains.com/phpstorm/2022/05/php-annotated-may-2022/",
+                "thubnamil" => [
+                    'src' => "https://blog.jetbrains.com/wp-content/uploads/2022/06/Blog_Featured_image_1280x600_PhpStorm-2x-1.png",
+                    'width'=> '2560',
+                    'height'=> '1200',
+                    'sizes' => '(max-width: 768px) 768px , 525px',
                 ],
-                [
-                    "href" => "https://blog.jetbrains.com/phpstorm/2022/05/php-annotated-may-2022/",
-                    "thubnamil" => [
-                        'src' => "https://blog.jetbrains.com/wp-content/uploads/2022/06/Blog_Featured_image_1280x600_PhpStorm-2x-1.png",
-                        'width'=> '2560',
-                        'height'=> '1200',
-                        'sizes' => '(max-width: 768px) 768px , 525px',
-                    ],
-                    "datetime" => '2022-06-13',
-                    "title" => 'PhpStorm 2022.2 EAP #3: Creating Enums',
-                    "description" => 'Welcome back to the 2022.2 EAP series! If you’re new or unsure what our Early Access Program is, make sure to read th…',
-                    "writer_img" => 'https://secure.gravatar.com/avatar/cef66d348f7def8f4634963a7e7a05e4?s=200&r=g',
-                    "writer" => 'Brent Roose',
-                ],
-                [
-                    "href" => "https://blog.jetbrains.com/phpstorm/2022/05/php-annotated-may-2022/",
-                    "thubnamil" => [
-                        'src' => "https://blog.jetbrains.com/wp-content/uploads/2022/06/Blog_Featured_image_1280x600_PhpStorm-2x-1.png",
-                        'width'=> '2560',
-                        'height'=> '1200',
-                        'sizes' => '(max-width: 768px) 768px , 525px',
-                    ],
-                    "datetime" => '2022-06-13',
-                    "title" => 'PhpStorm 2022.2 EAP #3: Creating Enums',
-                    "description" => 'Welcome back to the 2022.2 EAP series! If you’re new or unsure what our Early Access Program is, make sure to read th…',
-                    "writer_img" => 'https://secure.gravatar.com/avatar/cef66d348f7def8f4634963a7e7a05e4?s=200&r=g',
-                    "writer" => 'Brent Roose',
-                ]
-            ]
-        ];
-        return response($aaa, 200);
+                "datetime" => '2022-06-13',
+                "title" => 'PhpStorm 2022.2 EAP #3: Creating Enums',
+                "description" => 'Welcome back to the 2022.2 EAP series! If you’re new or unsure what our Early Access Program is, make sure to read th…',
+                "writer_img" => 'https://secure.gravatar.com/avatar/cef66d348f7def8f4634963a7e7a05e4?s=200&r=g',
+                "writer" => 'Brent Roose',
+            ]);
+        }
+        return response($data, 200);
     });
 });
