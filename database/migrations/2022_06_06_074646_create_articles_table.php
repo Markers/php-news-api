5eabfc8ba1a3aa96e023b5dd5edef912e0868ab2
Kyungseo-Park <troublesome.dev@gmail.com>
2022-06-26 15:02:34 +0900

    feat(db): datadabase update

--- database/migrations/2022_06_06_074646_create_articles_table.php
+++ database/migrations/2022_06_06_074646_create_articles_table.php
@@ -17,14 +17,27 @@ return new class extends Migration
         // 파일 데이터는 md와 mdx 파일로 관리
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->integer('post_id')->unique();
-            $table->enum('category', ['news', 'tutorials', 'videos', 'php-annotated-monthly','features', 'eap'])->index();
-            $table->string('thumbnail')->nullable();
-            $table->string('title');
-            $table->string('url');
-            $table->string('description');
-            $table->date('publish_date');
-            $table->boolean('is_translation')->default(false);
+            // Jetbrains 사이트에서 제공하는 정보를 저장한다.
+            $table->integer('post_id')->unique()->comment('웹 페이지의 고유 id');
+            $table->string('href')->comment('웹사이트에서의 글 주소');
+            $table->string('thumbnail')->nullable()->comment("썸네일 이미지");
+            $table->unsignedSmallInteger('width')->comment('width');
+            $table->unsignedSmallInteger('height')->comment('height');
+            $table->string('sizes')->nullable()->comment('css sizes');
+            $table->string('title')->comment("제목");
+            $table->string('description')->comment("설명");
+            $table->date('publish_date')->comment("작성일");
+            // 작성자 정보
+            $table->string('author')->comment("작성자");
+            $table->string('author_avatar')->nullable()->comment("작성자 아바타 URL");
+            // 구분을 위한 정보
+            $table->enum('category', ['news', 'tutorials', 'videos', 'php-annotated-monthly','features', 'eap'])->index()->comment('카테고리');
+            // 번역 정보를 제공
+            $table->string("slug")->nullable()->comment("슬러그");
+            $table->string("translated_title")->nullable()->comment("번역된 제목");
+            $table->string("translated_url")->nullable()->comment("번역된 파일 URL");
+            $table->string("translated_description")->nullable()->comment("번역된 설명");
+            $table->boolean('is_translation')->default(false)->comment("번역 상태");
             $table->timestamps();
         });
     }
