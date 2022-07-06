bc63d7081a2b423e8aa48d40d241d02d915c855f
Kyungseo-Park <troublesome.dev@gmail.com>
2022-07-07 02:53:04 +0900

    feat(lib): graham-campbell/github 프로바이더 등록

--- config/app.php
+++ config/app.php
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -212,6 +212,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'GitHub' => GrahamCampbell\GitHub\Facades\GitHub::class
     ])->toArray(),
 
 ];
