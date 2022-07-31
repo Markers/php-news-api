40a3080c110f97ffa4e38bc563732ba01e22242e
Kyungseo-Park <troublesome.dev@gmail.com>
2022-08-01 02:35:37 +0900

    feat: 사용하지 않는 api 주석함

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
 
     // 신규 포스트 목록을 크롤링 함 (내용 외 부가 정보)
-    Route::get("/crawling", [CrawlingController::class, 'getNewPostCrawling']);
+    // Route::get("/crawling", [CrawlingController::class, 'getNewPostCrawling']);
     // 내용을 가져 오지 않은 포스트 크롤링 함 (내용)
-    Route::get("/crawling/content", [CrawlingController::class, 'getContentCrawling']);
+    // Route::get("/crawling/content", [CrawlingController::class, 'getContentCrawling']);
     // 포스트 부가 정보 번역
-    Route::get('/translate', [TranslationController::class, 'translateTheDetails']);
+    // Route::get('/translate', [TranslationController::class, 'translateTheDetails']);
     // 포스트 내용 번역
-    Route::get('/translate/content', [TranslationController::class, 'translateTheContent']);
+    // Route::get('/translate/content', [TranslationController::class, 'translateTheContent']);
 
     // 포스트 목록을 가져 오는 API => 추후 API 내 페이징 추가 해야함
     Route::get('/articles', [ArticleController::class, 'allArticle']);
@@ -33,8 +33,8 @@ Route::prefix('v1')->group(function () {
     Route::get('/articles/{category}/{post_id}', [ArticleController::class, 'articlePost']);
 
     // 마크다운 생성
-    Route::get('markdown-format', [\App\Http\Controllers\MarkdownController::class, 'createMarkdown']);
+    // Route::get('markdown-format', [\App\Http\Controllers\MarkdownController::class, 'createMarkdown']);
 
     // 썸네일 생성을 위한 테스트 api
-    Route::get('test', [\App\Http\Controllers\MarkdownController::class, 'test']);
+    // Route::get('test', [\App\Http\Controllers\MarkdownController::class, 'test']);
 });
